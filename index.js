47fcc78d8e437c87243a8e4e1806499f7eaec6cb
youmekko <youmekko.o@gmail.com>
2020-07-12 23:31:29 +0900

    feat : register router 생성

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+//application/json
+app.use(bodyParser.json())
+
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://youmekko:1234@node-react-boilerplate.9xsyb.gcp.mongodb.net/node-react-boilerplate?retryWrites=true&w=majority', {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
@@ -9,5 +18,16 @@ mongoose.connect('mongodb+srv://youmekko:1234@node-react-boilerplate.9xsyb.gcp.m
 .catch(err=> console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
