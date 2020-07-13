594b4c2c987166331a3dc0c2b9c081ae1fdbf2cc
youmekko <youmekko.o@gmail.com>
2020-07-13 22:10:13 +0900

    chore : mongodb connection 정보 gitignore 파일로 변경

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = 5000
 const bodyParser = require('body-parser')
 const { User } = require('./models/User')
 
+const config = require('./config/key')
+
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}))
 
@@ -12,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://youmekko:1234@node-react-boilerplate.9xsyb.gcp.mongodb.net/node-react-boilerplate?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=> console.log('MongoDB Connected'))
 .catch(err=> console.log(err))
