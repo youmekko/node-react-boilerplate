5c9e1553c73ccff01a99cc35e15a7c5feec8a6bf
youmekko <youmekko.o@gmail.com>
2020-07-14 23:30:12 +0900

    feat : 로그인 구현 - JSONWEBTOKEN 생성

--- index.js
+++ index.js
@@ -3,17 +3,21 @@ const app = express()
 const port = 5000
 
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
 const { User } = require('./models/User')
 
 const config = require('./config/key')
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(cookieParser())
 
 //application/json
 app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
+
 mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=> console.log('MongoDB Connected'))
@@ -21,13 +25,43 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-
 app.post('/register', (req, res) => {
     const user = new User(req.body)
     user.save((err, userInfo) => {
         if(err) return res.json({success: false, err})
         return res.status(200).json({
             success: true
+        })
+    })
+})
+
+app.post('/login', (req, res) => {
+    User.findOne({email: req.body.email}, (err, user) => {
+
+        if(!user) {
+            return res.join({
+                loginSuccess: false,
+                message: "There is no user registered with that email address."
+            })
+        }
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch) {
+                return res.join({
+                    loginSuccess: false,
+                    message: "The password is wrong."
+                })
+            }
+    
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err) 
+                res.cookie('x_auth', user.token)
+                    .status(200)
+                    .json({
+                        loginSuccess: true,
+                        userId: user._id
+                    })
+            })
         })
     })
 })
