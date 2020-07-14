14b21e761c0f4fa0a2ebfec148920a418d428df9
youmekko <youmekko.o@gmail.com>
2020-07-15 08:03:32 +0900

    feat : auth 미들웨어 및  기능 추가

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const cookieParser = require('cookie-parser')
 const { User } = require('./models/User')
 
 const config = require('./config/key')
+const { auth } = require('./middleware/auth')
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}))
@@ -66,7 +67,17 @@ app.post('/api/users/login', (req, res) => {
     })
 })
 
-
-app.post('/api/users/')
+app.get('/api/users/auth', auth, (req, res) => {
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
