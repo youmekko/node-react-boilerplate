42aed6d0a3c1290e6a4f62fb1eca2cde4510eacc
ymchoi <ymchoi@wemb.co.kr>
2020-07-23 08:52:24 +0900

    fix : 오타 수정

--- client/src/_actions/user_actions.js
+++ client/src/_actions/user_actions.js
@@ -4,6 +4,8 @@ import { LOGIN_USER } from './types'
 export function loginUser(dataToSumbit) {
     const request = axios.post('/api/users/login', dataToSumbit)
         .then(res => res.data)
+
+    console.log("# request", request)    
     
     return {
         type: LOGIN_USER,
--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -3,6 +3,8 @@ import { LOGIN_USER } from '../_actions/types'
 export default function(prevState = {}, action) {
     switch (action.type) {
         case LOGIN_USER:
+            console.log("# prevStete", prevState)
+            console.log("# action.payload", action.payload)
             return {...prevState, loginSuccess: action.payload }
     
         default:
