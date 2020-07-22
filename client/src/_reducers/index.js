5f52c12de8c18ed18fe036947b2d86615ec3d1a0
youmekko <youmekko.o@gmail.com>
2020-07-23 00:22:57 +0900

    fix : 오타수정

--- client/src/_reducers/index.js
+++ client/src/_reducers/index.js
@@ -1,8 +1,8 @@
-import { combineReducer } from 'redux'
+import { combineReducers } from 'redux'
 import user from './user_reducer'
 
-const rootReducer = combineReducer({
+const rootReducer = combineReducers({
     user
 })
 
-export default reducer
+export default rootReducer
