6533142c507713f275fcd6179f2e43602cd038c9
youmekko <youmekko.o@gmail.com>
2020-07-23 00:23:31 +0900

    feat : 로그인 페이지 구현중

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Axios from 'axios'
 
 function LoginPage() {
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault()
+
+        console.log('Email', Email)
+        console.log('Password', Password)
+
+        let body = {
+            email: Email, 
+            password: Password
+        }
+
+        Axios.post('/api/users/login', body)
+            .then(res => {
+                
+            })
+    }
+
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height: '100vh'
+          }}>
+            <form style={{display: 'flex', flexDirection: 'column'}}
+                onSubmit={onSubmitHandler}>
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler} />
+                
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler} />
+                <br/>
+                <button>
+                    Login
+                </button>
+            </form>
         </div>
     )
+
 }
+
 
 export default LoginPage
